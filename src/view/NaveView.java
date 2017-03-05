abc05eab02abab823076e07fdc3fc5aec244bf18
Santiago Santana <santana.santiago@gmail.com>
2017-03-05 07:18:38 -0300

    La velocidad de la nave tiene el valor del spinner
    Funciona cambiar la velocidad usando el spinner.
    
    Para poder mover la nave luego de modificar el valor del spinner es
    necesario presionar tab.
    
    Probablemente sea buena idea poner el spinner en una ventana aparte, o
    separar la ventana en paneles.

--- src/view/NaveView.java
+++ src/view/NaveView.java
@@ -8,7 +8,10 @@ import model.NaveModel;
 import model.Observador;
 import java.awt.Color;
 import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+
 import java.awt.Font;
+import java.awt.event.KeyListener;
 
 
 public class NaveView implements Observador {
@@ -26,31 +29,54 @@ public class NaveView implements Observador {
 		this.naveModel= naveModel;
 		initialize();
 	}
+	
+	private void crearVentana() {
+		ventana = new JFrame();
+		ventana.getContentPane().setBackground(Color.BLACK); // cambio el color del fondo 
+		ventana.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ventana.getContentPane().setLayout(null);
+	}
+	
+	private int getValueSpinner() {
+		SpinnerNumberModel model = (SpinnerNumberModel) ctrVelocidad.getModel();
+		return model.getNumber().intValue();
+	}
+	
+	private void addSpinner() {
+		ctrVelocidad = new JSpinner();
+		ctrVelocidad.setBounds(10, 10, 153, 32);
+		ctrVelocidad.setValue(5);
+		
+		ctrVelocidad.addChangeListener(e -> naveModel.setVelocidad(getValueSpinner()));
+		
+		ventana.getContentPane().add(ctrVelocidad);
+	}
+	
+	private void addLabel() {
+		JLabel lblVelocidad = new JLabel("VELOCIDAD");
+		lblVelocidad.setFont(new Font("Tahoma", Font.PLAIN, 23));
+		lblVelocidad.setForeground(Color.RED);
+		lblVelocidad.setBounds(186, 14, 169, 15);
+		ventana.getContentPane().add(lblVelocidad);
+	}
+	
+	private void addNave() {
+		nave = new JLabel(Recursos.IMG_NAVE_ARRIBA);
+		nave.setBounds(33, 29, this.naveModel.getWidthNave(),this.naveModel.getHeightNave());
+		ventana.getContentPane().add(nave);
+		nave.requestFocus();
+	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 * @throws MalformedURLException 
 	 */
 	private void initialize() throws MalformedURLException {
-		ventana = new JFrame();
-		ventana.getContentPane().setBackground(Color.BLACK); // cambio el color del fondo 
-		ventana.setExtendedState(JFrame.MAXIMIZED_BOTH); 
-		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		ventana.getContentPane().setLayout(null);
-		
-		ctrVelocidad = new JSpinner();
-		ctrVelocidad.setBounds(10, 10, 153, 32);
-		ventana.getContentPane().add(ctrVelocidad);
-		
-		JLabel lblVelocidad = new JLabel("VELOCIDAD");
-		lblVelocidad.setFont(new Font("Tahoma", Font.PLAIN, 23));
-		lblVelocidad.setForeground(Color.RED);
-		lblVelocidad.setBounds(186, 14, 169, 15);
-		ventana.getContentPane().add(lblVelocidad);
-		
-		nave = new JLabel(new ImageIcon(NaveView.class.getResource("/images/naveUp.gif")));
-		nave.setBounds(33, 29, this.naveModel.getWidthNave(),this.naveModel.getHeightNave());
-		ventana.getContentPane().add(nave);
+		crearVentana();
+		addSpinner();
+		addLabel();
+		addNave();
 	}
 	
 	public void setVisible(boolean visible){
@@ -76,6 +102,11 @@ public class NaveView implements Observador {
 	
 	public void naveIzquierda() {
 		setIcon(Recursos.IMG_NAVE_IZQUIERDA);
+	}
+	
+	public void addKeyListener(KeyListener keyListener) {
+		ventana.setFocusable(true);
+		ventana.addKeyListener(keyListener);
 	}
 
 	//metodos de interfaces
