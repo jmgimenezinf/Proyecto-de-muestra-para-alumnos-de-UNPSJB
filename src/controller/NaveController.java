b8daffdbb7a6f386734b6b641b64a40a72fcab00
Santiago Santana <santana.santiago@gmail.com>
2017-03-05 01:56:13 -0300

    Logging al presionar teclas
    Pequeña refactorización en el método keyPressed del controlador.
    
    Añadidos algunos logs a consola para hacer más fácil la depuración.

--- src/controller/NaveController.java
+++ src/controller/NaveController.java
@@ -4,14 +4,17 @@ import java.awt.EventQueue;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.net.MalformedURLException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
 
 import model.NaveModel;
 import view.NaveView;
 
 public class NaveController implements KeyListener {
-
 	private NaveView unaNaveView;
 	private NaveModel unaNaveModel;
+	private static Logger logger = Logger.getLogger("NaveController");
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -37,19 +40,28 @@ public class NaveController implements KeyListener {
 		
 	}
 	
+	private boolean validKey(int key) {
+		List<Integer> validKeys = Arrays.asList(KeyEvent.VK_UP, KeyEvent.VK_DOWN, KeyEvent.VK_LEFT, KeyEvent.VK_RIGHT);
+		logger.info(String.format("Teclas Válidas: %s", validKeys));
+		return validKeys.contains(key);
+	}
+	
+	private void moverNave(int key) {
+		switch(key) {
+		case KeyEvent.VK_DOWN: unaNaveModel.abajo(); break;
+		case KeyEvent.VK_UP: unaNaveModel.arriba(); break;
+		case KeyEvent.VK_RIGHT: unaNaveModel.derecha(); break;
+		case KeyEvent.VK_LEFT: unaNaveModel.izquierda(); break;
+		}
+	}
+	
 	public void keyPressed(KeyEvent e) {
-
-		if( e.getKeyCode() == e.VK_DOWN) {
-			this.getUnaNaveModel().abajo();
-		}
-		if(e.getKeyCode()== e.VK_UP){
-			this.getUnaNaveModel().arriba();
-		}
-		if(e.getKeyCode() == e.VK_LEFT){
-			this.getUnaNaveModel().izquierda();
-		}
-		if(e.getKeyCode() == e.VK_RIGHT){
-			this.getUnaNaveModel().derecha();
+		int key = e.getKeyCode();
+		logger.info(String.format("Tecla Presionada: %d", key));
+		
+		if(validKey(key)) {
+			logger.info("¡Tecla Válida!");
+			moverNave(key);
 		}
 	}
 
@@ -72,7 +84,7 @@ public class NaveController implements KeyListener {
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		if( e.getKeyCode() == e.VK_A) {
+		if( e.getKeyCode() == KeyEvent.VK_A) {
 			this.getUnaNaveModel().pilotoAutomatico();
 		}
 	}
